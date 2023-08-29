affe5b3253bb04ed733b8bed3cefc5815154e1c7
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-08-29 15:29:43 +0200

    added styled description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-  return <div name="home" className="w-full h-screen bg-[#0a192f]">
-    <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p>Hi, my name is</p>
-        <h1>Katarzyna Zielińska</h1>
-        <h2>Software engineer</h2>
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-2xl py-2 text-pink-700">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Katarzyna Zielińska
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          Software engineer
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">specializing in building responsive web applications as well as mobile apps.</p>
         <div>
-            <button>View work <HiArrowNarrowRight></HiArrowNarrowRight></button>
+          <button className="text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-700 hover:border-pink-600">
+            View work <HiArrowNarrowRight className="ml-2"></HiArrowNarrowRight>
+          </button>
         </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Home;
--- src/components/Navbar/SocialIcon.jsx
+++ src/components/Navbar/SocialIcon.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const SocialIcon = ({ text, icon, color }) => {
-    const bgColorClass = color ? `bg-pink-700` : "bg-gray-700";
+    const bgColorClass = color ? `bg-${color}` : "bg-gray-700";
 
   return (
     <li
-      className={`w-[160px] h-[60px] flex justify-between items-center ml-[100px] hover:ml-[-10px] duration-300 ${bgColorClass}`}
+      className={`w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 ${bgColorClass}`}
     >
       <a
         className="flex justify-between items-center w-full text-gray-300"
