2290a55be421e108117b460b0220378a9c5ef8c7
twój login do GitHuba <jekaterina.zielinska@gmail.com>
2023-08-31 20:21:52 +0200

    added projects images + github links

---
+++ src/data/data.js
@@ -0,0 +1,85 @@
+import CarsharingImg from "../assets/carsharing.png";
+import ToDoAppImg from "../assets/todoapp.png";
+import EshopImg from "../assets/eshop.png";
+import GiveAwayAppImg from "../assets/giveawayapp.png";
+import ExpensifyImg from "../assets/expensify.png";
+import AddPlaceImg from "../assets/addplace.png";
+import HomeFlowImg from "../assets/homeflow.png";
+import MusicAppImg from "../assets/musicapp.png";
+import ChatAppImg from "../assets/chatapp.png";
+import HaveBiteImg from "../assets/haveabite.png";
+
+const data = [
+  {
+    id: 1,
+    name: "Giveaway App",
+    image: GiveAwayAppImg,
+    github: "https://github.com/kattziel/giveawayapp",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "Expensify",
+    image: ExpensifyImg,
+    github: "https://github.com/kattziel/expensify",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "eShop",
+    image: EshopImg,
+    github: "https://github.com/kattziel/eshop",
+    live: "",
+  },
+  // {
+  //   id: 4,
+  //   name: "ChatApp",
+  //   image: ChatAppImg,
+  //   github: "https://github.com/kattziel/chatapp",
+  //   live: "",
+  // },
+  {
+    id: 5,
+    name: "MusicApp",
+    image: MusicAppImg,
+    github: "https://github.com/kattziel/musicapp",
+    live: "",
+  },
+  {
+    id: 6,
+    name: "ToDoApp - company version",
+    image: ToDoAppImg,
+    github: "https://github.com/kattziel/todoapp",
+    live: "",
+  },
+  {
+    id: 7,
+    name: "Carsharing",
+    image: CarsharingImg,
+    github: "https://github.com/kattziel/carsharing",
+    live: "",
+  },
+  {
+    id: 8,
+    name: "Have a bite",
+    image: HaveBiteImg,
+    github: "https://github.com/kattziel/haveabite",
+    live: "",
+  },
+  {
+    id: 9,
+    name: "AddFavPlaces",
+    image: AddPlaceImg,
+    github: "https://github.com/kattziel/addfavplaces",
+    live: "",
+  },
+  {
+    id: 10,
+    name: "HomeFlow",
+    image: HomeFlowImg,
+    github: "https://github.com/kattziel/homeflow",
+    live: "",
+  },
+];
+
+export default data;
