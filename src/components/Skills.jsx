491f5828828360d2fbcd43a03c94f782667d4846
Katarzyna Zielinska <jekaterina.zielinska@gmail.com>
2023-09-30 10:51:49 +0200

    add new skills to portfolio

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,66 +2,73 @@ import React from "react";
 
 import JavaScript from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
-import RN from '../assets/RN1.jpg';
+import RN from "../assets/RN1.jpg";
 import Gql from "../assets/Gql.png";
 import Sass from "../assets/Sass.png";
 import TypeScriptImg from "../assets/Typescript.png";
 import FireBase from "../assets/firebase.png";
 import GitHub from "../assets/github.png";
 import Tailwind from "../assets/tailwind.png";
+import Redux from "../assets/redux.png";
 import Vsc from "../assets/vsc.png";
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-          <div>
-              <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
-              <p className='py-4'>Technologies I've worked with</p>
-          </div>
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
+            Skills
+          </p>
+          <p className="py-4">Technologies I've worked with</p>
+        </div>
 
-          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={JavaScript} alt="JS icon" />
-                  <p className='my-4'>JavaScript</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
-                  <p className='my-4'>React.js</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={RN} alt="React Native icon" />
-                  <p className='my-4'>React Native</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={TypeScriptImg} alt="TS icon" />
-                  <p className='my-4'>TypeScript</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Sass} alt="Sass icon" />
-                  <p className='my-4'>Sass</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={FireBase} alt="Firebase icon" />
-                  <p className='my-4'>Firebase</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind icon" />
-                  <p className='my-4'>Tailwind CSS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Gql} alt="GQL icon" />
-                  <p className='my-4'>GraphQL</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={GitHub} alt="Github icon" />
-                  <p className='my-4'>GitHub</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Vsc} alt="VSC icon" />
-                  <p className='my-4'>Visual Studio Code</p>
-              </div>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={JavaScript} alt="JS icon" />
+            <p className="my-4">JavaScript</p>
           </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React icon" />
+            <p className="my-4">React.js</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={RN} alt="React Native icon" />
+            <p className="my-4">React Native</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={TypeScriptImg} alt="TS icon" />
+            <p className="my-4">TypeScript</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Gql} alt="GQL icon" />
+            <p className="my-4">GraphQL</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Sass} alt="Sass icon" />
+            <p className="my-4">Sass</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={FireBase} alt="Firebase icon" />
+            <p className="my-4">Firebase</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon" />
+            <p className="my-4">Tailwind CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={GitHub} alt="Github icon" />
+            <p className="my-4">GitHub</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Redux} alt="Redux icon" />
+            <p className="my-4">Redux</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Vsc} alt="VSC icon" />
+            <p className="my-4">Visual Studio Code</p>
+          </div>
+        </div>
       </div>
     </div>
   );
